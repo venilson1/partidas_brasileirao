1debc44037f90d41000a0fe2ec3de2298fa8f544
venilson1 <venilson2009@hotmail.com>
2021-10-02 16:49:59 -0300

    page Championship

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
+import ChampionshipBrazilian from "./page/ChampionshipBrazilian";
 export default function App() {
   return (
-    <div>
-      <header>
-        <div className="bg-gray-100 mx-auto p-4">
-          <h1 className="text-center font-semibold text-xl">
-            Campeonato Brasileiro
-          </h1>
-        </div>
-      </header>
-
-      <main>
-        <div className="container mx-auto p-4">
-          <h2>O conteúdo fica aqui.</h2>
-        </div>
-      </main>
-    </div>
+    <>
+      <ChampionshipBrazilian />
+    </>
   );
 }
---
+++ src/page/ChampionshipBrazilian.jsx
@@ -0,0 +1,18 @@
+export default function ChampionshipBrazilian() {
+  return (
+    <div>
+      <header>
+        <div className="bg-gray-100 mx-auto p-4">
+          <h1 className="text-center font-semibold text-xl">
+            Campeonato Brasileiro
+          </h1>
+        </div>
+      </header>
+      <main>
+        <div className="container mx-auto p-4">
+          <h2>O conteúdo fica aqui.</h2>
+        </div>
+      </main>
+    </div>
+  )
+}
